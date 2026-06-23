1ed45c96ac717993359d7eed17978302d82664d8
yuzukiproject293-ctrl <yuzukiproject293@gmail.com>
2026-06-23 10:40:04 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'commissions-v1';
+const ASSETS = [
+  '/my-commissions/',
+  '/my-commissions/index.html',
+  '/my-commissions/manifest.json',
+  '/my-commissions/icon-192.png',
+  '/my-commissions/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
